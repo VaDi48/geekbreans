97c4dc280fe8d2592a34f8db23468b70632ded65
Lera Andreeva <valeraandreeva148@icloud.com>
2023-02-27 16:09:00 +0300

    task3

---
+++ task3.py
@@ -0,0 +1,3 @@
+user_num = int(input'Введите цифру месяца: ')
+sessons_dict = {1: 'Yanvar', 2:'Fevral', 3:'Mart', 4:'Aprel', 5:'May', 6:'Iiun', 7:'Iyul', 8:'Avgust', 9:'Sentyabr', 10:'Oktyabr', 11:'Noyabr', 12:'Decabr'}
+print(sessons_dict[user_num])
