bbd2c02f3902616ac955a9d24ef5d12aa181a239
Lera Andreeva <valeraandreeva148@icloud.com>
2023-03-09 16:22:50 +0300

    task1

--- task1.py
+++ task1.py
@@ -1,1 +1,10 @@
-print(hello) hh
+users_num = int(input('Введите числитель: '))
+users_den = int(input('Введите знаменатель: '))
+
+def devision_users_num(num1, num2):
+    result = num1/num2
+    if num2 == 0:
+        print('Делить на ноль нельзя')
+    else:
+        print(result)
+print(devision_users_num(users_num, users_den))
