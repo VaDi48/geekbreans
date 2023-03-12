a421b944d4d685b8e462fa47eadb016cbc992900
Lera Andreeva <valeraandreeva148@icloud.com>
2023-03-12 20:41:51 +0300

    task3

--- task3.py
+++ task3.py
@@ -1,8 +1,8 @@
 def my_func(num1, num2, num3):
-    my_list = [num1, num2, num3]
+    my_list = list([num1, num2, num3])
     try:
         my_list.remove(min(my_list))
         return sum(my_list)
-    except TypeError:
+    except (TypeError,ValueError):
         return 'Вводите только цифры'
 print(my_func(int(input('Введите первое значение: ')), int(input('Введите второе значение: ')), int(input('Введите третье значение: '))))
