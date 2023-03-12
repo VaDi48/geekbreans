33cb5faac56755a0d8c4debcaf9e0f461889f657
Lera Andreeva <valeraandreeva148@icloud.com>
2023-03-12 20:13:39 +0300

    task2

--- task2.py
+++ task2.py
@@ -1,10 +1,4 @@
-name = input('enter name: ')
-surname = input('enter surname: ')
-year = int(input('enter year: '))
-city = input('enter city: ')
-email = input('enter email: ')
-telephone = input('input telephone: ')
+def users_info(**kwargs):
+     return ' '.join(**kwargs.values())
 
-def my_func (name, surname, year, city, email, telephone):
-     return ' '.join([name, surname, year, city, email, telephone])
-print(my_func(surname = surname, name = name, year = year, city = city, email = email, telephone = telephone))
+print(users_info(name = input('Введите свое имя: '), surname = input(' Введите свою фамилию: '), city = input('Введите город проживания: '), phone = input(' Введите номер телефона: '), year = input('Введите год рождения: ')))
